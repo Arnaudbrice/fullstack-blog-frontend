05f97e1ff4104d126c0a20c19abde5a3d701f8fe
arnaudbrice <mephisteur@gmail.com>
2025-08-21 17:17:53 +0200

    add margin to the grid of the Home Component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,17 +2,37 @@ import React from "react";
 import { Outlet } from "react-router";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { Bounce, ToastContainer } from "react-toastify";
 
 const Layout = () => {
   return (
-    <div>
-      <Header />
+    <>
+      <div>
+        <Header />
+
+        <ToastContainer
+          className="mt-16 text-lg"
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          limit={2}
+          transition={Bounce}
+        />
+      </div>
+
       <main>
         <Outlet />
       </main>
 
       <Footer />
-    </div>
+    </>
   );
 };
 
